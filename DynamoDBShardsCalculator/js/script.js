90060b7efb6c76cd2af677ac5dcefe0d34debf15
Nathaniel Felsen <n@medium.com>
2016-07-08 14:51:38 -0700

    Updating script to reflect new formula

--- DynamoDBShardsCalculator/js/script.js
+++ DynamoDBShardsCalculator/js/script.js
@@ -7,7 +7,7 @@ $(document).ready(function () {
     var storage = $("#storage");
     var pricing = $("#pricing");
     var calculateBasedOnSize = function () {
-        return Math.max(1,Math.ceil(parseFloat(size.val()) / 25));
+        return Math.max(1,Math.ceil(parseFloat(size.val()) / 10));
     };
     var calculateBasedOnThroughput = function () {
         return Math.max(1,Math.ceil(parseFloat(rcu.val()) / 3000 + parseFloat(wcu.val()) / 1000));
@@ -34,11 +34,11 @@ $(document).ready(function () {
         if ( !isNaN(r[1])) {
             rwcu.text(txtThroughput);
         }
-        var txtTotal = "Total number of shards allocated by DynamoDB: " + r[2];
+        var txtTotal = "Total number of shards allocated (Max(shared for throughput, shards for capacity)): " + r[2];
         if ( !isNaN(r[2])) {
             output.text(txtTotal);
         }
-        var txtPricing = "Pricing in us-east-1: $" + r[3][0] + " per hour ($" + r[3][1] + " monthly)";
+        var txtPricing = "Pricing in us-east-1 (might be subject to change): $" + r[3][0] + " per hour ($" + r[3][1] + " monthly)";
         if ( !isNaN(r[3][0])) {
             pricing.text(txtPricing);
         }
